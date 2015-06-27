ff8b8198e3ffb6224d4ff1dfde60f9e697233859
scirelli <scirelli+git@gmail.com>
2015-06-26 21:19:39 -0400

    First commit

---
+++ ButtonTry.java
@@ -0,0 +1,39 @@
+import java.applet.*;
+import java.awt.*;
+
+
+public class ButtonTry extends Applet
+{
+	Button okButton = new Button("Ok");
+	private int mouseX, mouseY;
+	private boolean mouseclick= false;
+					  
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		g.drawRect(50,20,100,100);
+		
+	}
+	
+	public boolean mouseMove(Event e, int x, int y)
+	{
+		mouseX= x;
+		mouseY= y;
+		return true;
+	}
+	
+	public boolean mouseDown(Event p1, int p2, int p3)
+	{
+		mouseclick= true;
+		repaint();
+		return true;
+	}
+	
+	public void init()
+	{
+		BorderLayout BorderlayBs = new BorderLayout(10,15);		setLayout(BorderlayBs);
+		add("South",okButton);
+				super.init();
+	}
+	
+}
